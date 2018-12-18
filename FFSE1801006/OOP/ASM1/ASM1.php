fd89dee7004d9fe833468012c64c80f9c8936de1
Fanguyen02 <42628530+Fanguyen02@users.noreply.github.com>
2018-12-18 20:42:43 +0700

    g

---
+++ FFSE1801006/OOP/ASM1/ASM1.php
@@ -0,0 +1,68 @@
+<!-- Xây dựng lớp hình tròn dẫn xuất từ lớp điểm, kế thừa phương thức của lớp điểm -->
+
+<html>
+    <body>
+
+        
+        <?php
+            class POINT {
+                private $x = '' ;
+                private $y = '' ;
+
+                public function __construct($x,$y) {
+                    $this->x = $x;
+                    $this->y = $y;
+ 
+                }
+
+                function show_xy() {
+                    echo $this->x;
+                    echo "<br>";
+                    echo $this->y;
+
+                }
+
+            }
+
+            class CIRCLE extends POINT{
+                private $r = '' ;
+
+                public function __construct() {
+                    parent::__construct;
+                    $this->r = $r;
+
+                }
+
+                public function show_r() {
+
+                    parent::show_xy();
+                    echo "<br>";
+                    echo $this->r;
+                }
+
+            }
+            
+            // <!-- <form action="" method="POST">
+            //     <input type="text" name="x" placeholder="x"/>
+            //     <input type="text" name="y" placeholder="y"/>
+            //     <input type="submit" name="sub" value="Gửi"/>
+
+            // </form> -->
+            
+            // if(isset($_POST['sub'])) {
+            //     $diem = new POINT($_POST['x'],$_POST['y']);
+            //     echo '<pre>';
+            //     $diem ->show_r();
+            //     echo '</pre>';
+
+                
+
+            // }
+                
+            $diem = new CIRCLE(23,65,7);
+            show_r();
+        ?>  
+
+        
+    </body>
+</htmL>
