362d54092dc61a0488975aec8a6a13c452d79cc7
Trần Ngọc Dân <FFSE1801002@ST.fasttrack.edu.vn>
2018-12-27 00:44:45 +0700

    cv

--- FFSE1801002/CV/page/trangchu.php
+++ FFSE1801002/CV/page/trangchu.php
@@ -1,73 +1,63 @@
 <!DOCTYPE html>
 <html lang="vi">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="SHORTCUT ICON" href="../img/icon.png">
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /> 
 	<title>CV</title>
-	<link rel="stylesheet" href="../css/bootstrap.css">
-	<link rel="stylesheet" href="../css/style.css">
-	<link rel="stylesheet" href="../icon 7 stroke/Pe-icon-7-stroke.css">
-	<link rel="stylesheet" href="../icon 7 stroke/helper.css">
+	<link rel="SHORTCUT ICON" href="../img/icon.png" />
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link href='http://fonts.googleapis.com/css?family=Electrolize' rel='stylesheet' type='text/css' />
+	<link rel="stylesheet" type="text/css" href="../css/style.css" />
+	<link rel="stylesheet" href="../icon 7 stroke/Pe-icon-7-stroke.css" />
+	<link rel="stylesheet" href="../icon 7 stroke/helper.css" />
 </head>
 <body>
-	<div>
-		<div class="menu">
-			<ul>
-				<li><a href=""><img src="../img/logo.png"></a></li>
-				<li>
-					<a href="">
-						<i class="pe-7s-home pe-2x pe-va"></i>
-						<span class="menu1">Home</span>
-						<span class="menu2"></span>
-					</a>
-				</li>
-				<li>
-					<a href="">
-						<i class="pe-7s-user pe-2x pe-va"></i>
-						<span class="menu1">Home</span>
-						<span class="menu2"></span>
-					</a>
-				</li>
-				<li>
-					<a href="">
-						<i class="pe-7s-home pe-2x pe-va"></i>
-						<span class="menu1">Home</span>
-						<span class="menu2"></span>
-					</a>
-				</li>
-				<li>
-					<a href="">
-						<i class="pe-7s-home pe-2x pe-va"></i>
-						<span class="menu1">Home</span>
-						<span class="menu2"></span>
-					</a>
-				</li>
-				<li>
-					<a href="">
-						<i class="pe-7s-home pe-2x pe-va"></i>
-						<span class="menu1">Home</span>
-						<span class="menu2"></span>
-					</a>
-				</li>
-				<li>
-					<a href="">
-						<i class="pe-7s-mail pe-2x pe-va"></i>
-						<span class="menu1">Home</span>
-						<span class="menu2"></span>
-					</a>
-				</li>
-			</ul>
-		</div>
-		<div class="home">
-			<div>
-				
-			</div>
-		</div>
-
-
-	</div>
-	<script src="../js/jquery-3.3.1.js"></script>
-	<script src="../js/bootstrap.min.js"></script>
+	<div id="1" class="panel"></div>
+	<div id="2" class="panel"></div>
+	<div id="3" class="panel"></div>
+	<div id="4" class="panel"></div>
+	<div id="5" class="panel"></div>
+	<div id="6" class="panel"></div>
+	<div class="menu">
+		<ul>
+			<li><a href=""><img src="../img/logo.png"></a></li>
+			<li>
+				<a href="#1">
+					<i class="pe-7s-home pe-2x pe-va"></i>
+					<span class="menu1">Home</span>
+				</a>
+			</li>
+			<li>
+				<a href="#2">
+					<i class="pe-7s-user pe-2x pe-va"></i>
+					<span class="menu1">Home</span>
+				</a>
+			</li>
+			<li>
+				<a href="#3">
+					<i class="pe-7s-home pe-2x pe-va"></i>
+					<span class="menu1">Home</span>
+				</a>
+			</li>
+			<li>
+				<a href="#4">
+					<i class="pe-7s-home pe-2x pe-va"></i>
+					<span class="menu1">Home</span>
+				</a>
+			</li>
+			<li>
+				<a href="#5">
+					<i class="pe-7s-home pe-2x pe-va"></i>
+					<span class="menu1">Home</span>
+				</a>
+			</li>
+			<li>
+				<a href="#6">
+					<i class="pe-7s-mail pe-2x pe-va"></i>
+					<span class="menu1">Home</span>
+				</a>
+			</li>
+		</ul>
+	</div>      
 </body>
 </html>
