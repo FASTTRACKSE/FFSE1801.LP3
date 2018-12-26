d0e0b79e9ae653c7168de871175d033ae9f069fe
Trần Ngọc Dân <FFSE1801002@ST.fasttrack.edu.vn>
2018-12-26 13:57:37 +0700

    fix

---
+++ FFSE1801002/CV/page/trangchu.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="SHORTCUT ICON" href="../img/icon.png">
+	<title>CV</title>
+	<link rel="stylesheet" href="../css/bootstrap.css">
+	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="../icon 7 stroke/Pe-icon-7-stroke.css">
+	<link rel="stylesheet" href="../icon 7 stroke/helper.css">
+</head>
+<body>
+	<div>
+		<div class="menu">
+			<ul>
+				<li><a href=""><img src="../img/logo.png"></a></li>
+				<li>
+					<a href="">
+						<i class="pe-7s-home pe-2x pe-va"></i>
+						<span class="menu1">Home</span>
+						<span class="menu2"></span>
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<i class="pe-7s-user pe-2x pe-va"></i>
+						<span class="menu1">Home</span>
+						<span class="menu2"></span>
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<i class="pe-7s-home pe-2x pe-va"></i>
+						<span class="menu1">Home</span>
+						<span class="menu2"></span>
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<i class="pe-7s-home pe-2x pe-va"></i>
+						<span class="menu1">Home</span>
+						<span class="menu2"></span>
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<i class="pe-7s-home pe-2x pe-va"></i>
+						<span class="menu1">Home</span>
+						<span class="menu2"></span>
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<i class="pe-7s-mail pe-2x pe-va"></i>
+						<span class="menu1">Home</span>
+						<span class="menu2"></span>
+					</a>
+				</li>
+			</ul>
+		</div>
+		<div class="home">
+			<div>
+				
+			</div>
+		</div>
+
+
+	</div>
+	<script src="../js/jquery-3.3.1.js"></script>
+	<script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
